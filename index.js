92fe64937d86887bad7edda0078e2789de3b4bbb
Caitlin Keefe <caitlinkeefe@thinkful.com>
2020-07-21 19:47:56 -0400

    created js

--- index.js
+++ index.js
@@ -2,15 +2,25 @@ $(function() {
     $('.container').submit(event => {
         event.preventDefault();
         const containerText = $(event.currentTarget).find('#shopping-list-entry');
-        $(".shopping-item").text(`${containerText.val()}`);
+        $("ul").append(`<li>
+        <span class="shopping-item">${containerText.val()}</span>
+        <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+    <span class="button-label">check</span>
+  </button>
+            <button class="shopping-item-delete">
+    <span class="button-label">delete</span>
+  </button>
+        </div>
+    </li>`);
         containerText.val("");
     });
-    $("button.submit").click(function(event) {
-        $("ul").append('<li>' + $({
-                containerText.val())
-        })
-        $('ul').on('click', 'li', function(event) {
-            this.remove();
-        });
+    //delete item
+    $(".shopping-list").on('click', ".shopping-item-delete", function() {
+        $(this).closest("li").remove();
+    });
+    //check item
+    $(".shopping-list").on("click", ".shopping-item-toggle", function() {
+        $(this).closest("li").find(".shopping-item").toggleClass("shopping-item__checked");
     });
 });
